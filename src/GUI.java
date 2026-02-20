5b68736c78ec4bdce4a4f488a9e2942bf04bf830
sara2 <sara270498@gmail.com>
2026-02-20 16:19:47 +0200

    random stuff

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class GUI {
@@ -28,20 +29,31 @@ public class GUI {
             button.setHorizontalTextPosition(SwingConstants.CENTER);
         }
 
-        button.setFont(new Font("Segoe UI Emoji", Font.BOLD, 20));
+        button.setFont(new Font("Segoe UI Emoji", Font.ROMAN_BASELINE, 25));
         button.setForeground(Color.BLACK);
-        button.setBackground(Color.white);
+
+
 
         button.addActionListener(e -> {
             count++;
             System.out.println("The cat was clicked! Total: " + count);
 
-            if(count > 5){
+            if(count == 5 || count == 6){
                 button.setText("WHY DID YOU DO THAT");
                 button.setBackground(Color.RED);
                 button.setForeground(Color.white);
             }
-            else{
+            else if(count == 7){
+                button.setText("why did you stop");
+                button.setBackground(Color.GREEN);
+                button.setForeground(Color.BLACK);
+
+            } else if (count > 7) {
+                button.setText("why didnt you stop");
+                button.setBackground(Color.RED);
+
+
+            } else{
                 button.setText("AGAIN!🐱");
                 button.setBackground(new Color(144, 238, 144));
 
@@ -49,17 +61,23 @@ public class GUI {
 
 
         });
-        // הגדרות למראה מודרני
+
         button.setUI(new javax.swing.plaf.basic.BasicButtonUI());
         button.setContentAreaFilled(true);
         button.setOpaque(true);
-        button.setBorderPainted(false);
         button.setFocusPainted(false);
         button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        button.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
-        // הוספה לחלון
+
+        button.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.BLACK, 2),
+                BorderFactory.createEmptyBorder(5, 15, 5, 15)
+        ));
+
+
         frame.setLayout(new GridBagLayout());
-        frame.getContentPane().setBackground(new Color(6, 19, 8));
+        frame.getContentPane().setBackground(new Color(21, 53, 8, 255));
         frame.add(button);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 350);
